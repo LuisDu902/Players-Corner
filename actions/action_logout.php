dbd045ec46e62a41bd43e841947a0ba289fa3e34
Luis Du <up202105385@up.pt>
2023-04-17 21:38:32 +0100

    action_logout implemented

---
+++ actions/action_logout.php
@@ -0,0 +1,9 @@
+<?php
+  declare(strict_types = 1);
+
+  require_once(__DIR__ . '/../classes/session.class.php');
+  $session = new Session();
+  $session->logout();
+
+  header('Location: ../pages/index.php');
+?>
