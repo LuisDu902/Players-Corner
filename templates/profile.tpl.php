31c7868669c6f6bd1d5ae1dbbc89445871fe8354
Luis Du <up202105385@up.pt>
2023-04-20 03:00:16 +0100

    edit profile done

--- templates/profile.tpl.php
+++ templates/profile.tpl.php
@@ -1,50 +1,78 @@
 <?php function drawProfile(User $user)
 { ?>
-    <div class="user-profile-container">
-        <div class="user-profile round-wrap">
-            <img src="../images/profile.png" alt="user-profile">
-            <span> <?=$user->username?> </span>
-            <a href="../pages/profile.edit.php" id="edit-button"> Edit profile</a>
+    <div class="user-profile">
+
+        <div class="user-reputation">
+            <span id="reputation" class="title">Reputation</span>
+            <span class="reputation-value">
+                <?= $user->reputation ?>%
+            </span>
         </div>
         <div class="user-details">
-            <span id="reputation">Reputation</span>
-            <span class="reputation-value"><?= $user->reputation ?>%</span>
-            <div class = "data">
-                <span class="field"> Name </span>
-                <span class="info"> <?= $user->name?> </span> 
-                <span class="field"> Username </span>
-                <span class="info"> <?= $user->username?> </span> 
-                <span class="field"> Email </span>
-                <span class="info"> <?= $user->email?> </span> 
-            </div>
+            <span id="about" class="title">About me</span>
+            <span class="field"> Name </span>
+            <span class="info">
+                <?= $user->name ?>
+            </span>
+            <span class="field"> Username </span>
+            <span class="info">
+                <?= $user->username ?>
+            </span>
+            <span class="field"> Email </span>
+            <span class="info">
+                <?= $user->email ?>
+            </span>
+            <span class="field"> Role </span>
+            <span class="info">
+                <?= $user->type ?>
+            </span>
         </div>
-        <div class="last-ticket">
-            <span id="ticket">Last ticket</span>
-            <div>
-                <img src="../images/giphy3.gif">
-            </div>
+        <div class="profile-picture round-wrap">
+            <img src="../images/profile.png" alt="user-profile">
+            <span>
+                <?= $user->username ?>
+            </span>
+            <a href="../pages/profile.edit.php" id="edit-button"> Edit profile</a>
         </div>
-       
     </div>
-   
+
 <?php } ?>
 
-<?php function drawEditUserForm() { ?>
-    <section id="edit-profile">
-        <h1>Edit profile</h1>
-        <form action="../actions/action_edit_profile.php" method="post">
-            <label>Name: <input type="text" name="name" required="required" value="<?=htmlentities($_SESSION['input']['nome oldUser'])?>"></label>
-            <label>Username: <input type="text" name="username" required="required" value="<?=htmlentities($_SESSION['input']['morada oldUser'])?>"></label>
-            <label>Email: <input type="email" name="email" required="required" value="<?=$_SESSION['input']['telemovel oldUser']?>"></label>
-            <label>Password: <input type="text" name="password" required="required" value="<?=htmlentities($_SESSION['input']['email oldUser'])?>"></label>
-            <label>New password: <input type="new-password" name="password1"></label>
-            <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>">
-            <input id="button" type="submit" value="Concluir edição" >
-        </form>
-        <form action="../actions/uploadProfileImage.action.php" method="post" enctype="multipart/form-data">
-          <label>Profile photo: <input type="file" name="image"></label>
-          <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>">
-          <input type="submit" value="Upload">
-        </form>
-    </section> 
+<?php function drawEditUserForm()
+{ ?>
+    <div class="user-profile round-wrap">
+        <div class="edit-profile">
+            <h2 class="register-text">Edit profile</h2>
+            </h2>
+            <form action="../actions/action_edit_profile.php" method="post" class="authentication-form">
+                <div class="input-box">
+                    <input type="text" name="name" required="required" placeholder="Name">
+                    <img src="../images/user.png" class="icon" alt="user">
+                </div>
+
+                <div class="input-box">
+                    <input type="email" name="email" required="required" placeholder="Email">
+                    <img src="../images/email.png" class="icon" alt="email">
+                </div>
+
+                <div class="input-box">
+                    <input type="password" name="password" required="required" placeholder="Password">
+                    <img src="../images/password.png" class="icon" alt="password">
+                </div>
+
+                <div class="input-box">
+                    <input type="username" name="username" required="required" placeholder="Username">
+                    <img src="../images/username.png" class="icon" alt="username">
+                </div>
+                <button type="submit" class="authentication-button">Edit</button>
+            </form>
+        </div>
+        <div class="upload-photo round-wrap">
+            <form action="../actions/action_upload_image.php" method="post" enctype="multipart/form-data" class="authentication-form">
+                <img src="../images/profile.png" alt="user-profile">
+                <input type="hidden" name="csrf" value="<?= $_SESSION['csrf'] ?>">
+                <button type="submit" value="upload" class="authentication-button" id="upload">Upload photo</button>
+            </form>
+        </div>
+    </div>
 <?php } ?>
