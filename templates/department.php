172ac8df0df977d92ab0ff7034d83eb4df2480d4
Luis Du <up202105385@up.pt>
2023-04-23 16:55:45 +0100

    adding popu

--- templates/department.php
+++ templates/department.php
@@ -2,16 +2,35 @@
 { ?>
     <div class="departments-bar">
         <span>Departments</span>
-        <button type="submit">Add new department</button>
-  </div>
-    <div class = "departments">
-    <?php foreach ($departments as $department): ?>
-        
-        <a href="../pages/index.php"  class="department">
-            <img src="../images/departments/image.png" alt="department image"></img>
-            <span><?=$department->category?></span>
-        </a>
-    <?php endforeach; ?>
+        <button id="myBtn">Add new department</button>
     </div>
-    
+
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <form action="add_department.php" method="post" enctype="multipart/form-data">
+                <label for="name">Department Name:</label>
+                <input type="text" id="name" name="name"><br><br>
+                <label for="image">Department Image:</label>
+                <input type="file" id="image" name="image"><br><br>
+                <input type="submit" value="Add Department">
+            </form>
+        </div>
+
+    </div>
+
+    <div class="departments">
+        <?php foreach ($departments as $department): ?>
+
+            <a href="../pages/index.php" class="department">
+                <img src="../images/departments/image.png" alt="department image"></img>
+                <span>
+                    <?= $department->category ?>
+                </span>
+            </a>
+        <?php endforeach; ?>
+    </div>
+
 <?php } ?>
