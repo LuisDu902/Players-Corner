b5fe20ed6a1eea0381d769b667641f31cbbaa716
Madalena Ye <mads1040@gmail.com>
2023-05-07 13:24:22 +0100

    modifying margins

--- templates/initial.php
+++ templates/initial.php
@@ -1,14 +1,17 @@
 <?php function drawInitial()
 { ?>
-    <div class="banner">
-        <div class="corner-text">
+    <section id="container">
+        <article class="corner-text">
             <h1>Player's Corner</h1>
             <p><br>The one-stop-shop for all your gaming FAQs and strategies. <br> No gamer should be left out!</p>
-        </div>
-        <div class="gif">
+        </article>
+        <article class="gif">
             <img src="../images/banner.gif">
-        </div>
-    </div>
+        </article>
+    </section>
+    <section id="popular-posts">
+         <h1>Popular Posts</h1>
+    </section>
 <?php }
 
 
