de3c909e01f1f673d3bd0b9ca9c06e4649240900
Madalena Ye <mads1040@gmail.com>
2023-05-09 21:23:04 +0100

    updating main page, still on going

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="../css/modal.css">
     <link rel="stylesheet" href="../css/create_ticket.css">
     <link rel="stylesheet" href="../css/token-input.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js" integrity="sha512-Wt1bJGtlnMtGP0dqNFH1xlkLBNpEodaiQ8ZN5JLA5wpc1sUlk/O5uuOMNgvzddzkpvZ9GLyYNa8w2s7rqiTk5Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="../javascript/add_new_department.js" defer></script>
     <script src="../javascript/assign_to_departments.js" defer></script>
--- templates/initial.php
+++ templates/initial.php
@@ -4,15 +4,46 @@
         <article class="corner-text">
             <h1>Player's Corner</h1>
             <p><br>The one-stop-shop for all your gaming FAQs and strategies. <br> No gamer should be left out!</p>
+            <a href="#" class="btn">About us</a>
         </article>
         <article class="gif">
             <img src="../images/banner.gif">
         </article>
     </section>
-    <section id="popular-posts">
-         <h1>Popular Posts</h1>
+    <section class="categories">
+         <h1 class="heading-title">Categories</h1>
+         <div class="container">
+            <div class="product">
+                <div class="effect-1"></div>
+                <div class="effect-2"></div>
+                <div class="content">
+                    <i class='bx bxl-dev-to' ></i>
+                </div>
+                <span class="title">
+                    Game Development
+                </span>
+            </div>
+            <div class="product">
+                <div class="effect-1"></div>
+                <div class="effect-2"></div>
+                <div class="content">
+                    <i class='bx bxl-dev-to' ></i>
+                </div>
+                <span class="title">
+                Tech Support
+                </span>
+            </div>
+            <div class="product">
+                <div class="effect-1"></div>
+                <div class="effect-2"></div>
+                <div class="content">
+                    <i class='bx bxl-dev-to' ></i>
+                </div>
+                <span class="title">
+                    Game Design
+                </span>
+            </div>
+        </div>
     </section>
-<?php }
-
-
-
+<?php
+}
