533a1bd8c851b483b07320b3003609ad9016a65c
Madalena Ye <mads1040@gmail.com>
2023-05-16 03:13:53 +0100

    changing header

--- templates/initial.php
+++ templates/initial.php
@@ -3,7 +3,10 @@
 <?php function drawInitial($session){ ?>
     <section id="container">
         <article class="corner-text">
-            <h1>Player's Corner</h1>
+            <h1>
+                This is <br>
+                <span class="auto-type"></span>
+            </h1>
             <p><br>The one-stop-shop for all your gaming FAQs and strategies. <br> No gamer should be left out!</p>
             <a href="#" class="btn">About us</a>
         </article>
@@ -20,5 +23,15 @@
     } ?>
          
     </section>
+</body>
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+<script>
+        var typed = new Typed('.auto-type', {
+      strings: ["Player's Corner", "Adventure", "Experience", "Comfort"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true
+    });
+</script>
 <?php
 }
