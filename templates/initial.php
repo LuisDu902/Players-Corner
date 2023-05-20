bec0cdbacb53e5af9c98526592b686f75e6d0921
LuisDu902 <up202105385@edu.fe.up.pt>
2023-05-20 08:10:19 +0100

    adding about section

--- templates/initial.php
+++ templates/initial.php
@@ -17,6 +17,15 @@
     <section class="categories">
         <?php drawDepartments($session, $departments); ?>
     </section>
+    <section id="about-us">
+        <h2 class="heading-title">About us</h2>
+        <article class="about-content">
+            <img src="../images/about-us.jpeg" alt="about us banner" class="white-border round-border">
+            <p>
+            Welcome to Player's Corner, the ultimate destination for all your gaming FAQs and strategies. At Player's Corner, we believe that no gamer should be left out when it comes to finding solutions to their gaming troubles. Our dedicated team of experts is here to provide you with a one-stop-shop experience, offering comprehensive and reliable support for all your gaming needs. Whether you're stuck on a challenging level, seeking advice on game strategies, or looking for answers to common gaming queries, Player's Corner is your go-to resource. With our extensive collection of FAQs, helpful guides, and a vibrant community of fellow gamers, you'll never have to face gaming challenges alone. Join us at Player's Corner and unlock a world of knowledge, tips, and tricks to level up your gaming experience.
+            </p>
+        </article>
+    </section>
 <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js" defer></script>
 <script src="../javascript/typed.js" defer></script>
 <?php } ?>
